7bf0d5c3fbf92f52823187d97865afb4c938470a
T-Mills <77327041+T-Mills@users.noreply.github.com>
2021-02-08 17:08:20 +0000

    Update to include input
    As required by the coursework, user will be prompted for their name and this will be used in the ouput

--- HelloWorldCm1705.py
+++ HelloWorldCm1705.py
@@ -6,4 +6,6 @@ Created on Mon Feb  8 16:59:24 2021
 """
 # This is the program required for CM1705 CW2 Q1.
 
-print ("Hello, world!")
+user_name = input("Please input your name: )
+
+print ("Hello", user_name, "!")
